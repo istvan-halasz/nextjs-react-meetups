29ecd77430cf23bf9ed89d3241e0d2a16a7a9945
istvan-halasz <haistvan@gmail.com>
2023-03-05 19:46:26 +0200

    Getting Meetup Details Data & Preparing Pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,32 +1,35 @@
 import MeetupDetail from '@/components/meetups/MeetupDetail';
+import { MongoClient, ObjectId } from 'mongodb';
 
 function MeetUpDetailsPage(props) {
   return (
     <MeetupDetail
       image={props.meetupData.image}
-      title="First Meetup"
-      address="Some Street 5, Some City"
-      description="This is a first meetup"
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
 export default MeetUpDetailsPage;
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://Isti:kCdUzjNIMynTJAjF@cluster1.998sros.mongodb.net/meetups?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetupIds = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: 'm1',
-        },
-      },
-      {
-        params: {
-          meetupId: 'm2',
-        },
-      },
-    ],
+    paths: meetupIds.map((m) => ({
+      params: { meetupId: m._id.toString() },
+    })),
   };
 }
 
@@ -35,17 +38,27 @@ export async function getStaticProps(context) {
 
   const meetupId = context.params.meetupId;
 
+  const client = await MongoClient.connect(
+    'mongodb+srv://Isti:kCdUzjNIMynTJAjF@cluster1.998sros.mongodb.net/meetups?retryWrites=true&w=majority'
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: new ObjectId(meetupId),
+  });
+
   console.log(meetupId);
 
   return {
     props: {
       meetupData: {
-        image:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-        id: meetupId,
-        title: 'First Meetup',
-        address: 'Some Street 5, Some City',
-        description: 'This is a first meetup',
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
       },
     },
   };
